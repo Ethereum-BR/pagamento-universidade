d24aec8ecf537984fe17136d43a28a7cb82dc47a
Marcelo Creimer <14227072+mcreimer@users.noreply.github.com>
2018-02-09 17:07:26 -0200

    Correção da verificação de saldo
    Alterações:
    1) Mudei o nome do evento de "Enviado" para "UnicFoiPaga", apenas para maior clareza
    2) Incluí o método construtor, por boa prática e evitar warnings
    3) Corrigi um erro sério: eu estava testando se quem (o endereço) disparava a função tinha pelo menos 1 Ether de saldo.  Mas na verdade, quem deve ter esse saldo é o próprio contrato!

--- pagamentoUNIC.sol
+++ pagamentoUNIC.sol
@@ -4,14 +4,17 @@ contract pagamentoUNIC {
 
   address public enderecoUNIC = 0x8977C1F513ca4C60d331B027911AFd23Aa370557;  // UNIC = University of Nicosia
   uint dataUltimaTransferencia;
-  event Enviado(address remetente, address destinatario, uint valor);
+  event UnicFoiPaga(address enderecoQueDisparouContrato, address destinatario, uint valor);
 
+  function pagamentoUNIC() public payable {
+  }
+  
   function transfereParaUNIC() public payable {
   	address enderecoRemetente = msg.sender;
   	uint valorATransferir = 1 ether;
 
-    	// Se Paulo (msg.sender) nao tiver saldo de pelo menos 1 Ether, a função não fará nada
-  	require(enderecoRemetente.balance >= valorATransferir);
+    	// Se o contrato nao tiver saldo de pelo menos 1 Ether, a função não fará nada
+  	require(this.balance >= valorATransferir);
 
 	// Se a ultima transferencia foi feita a menos de 30 dias, não permitir nova transferencia  	 
 	require(now >= dataUltimaTransferencia + 30 days); 
@@ -19,7 +22,7 @@ contract pagamentoUNIC {
    	enderecoUNIC.transfer(valorATransferir);  // Caso não consiga transferir, gera erro e não prossegue
    	
 	dataUltimaTransferencia = now;	
-	Enviado(msg.sender, enderecoUNIC, valorATransferir);
+	UnicFoiPaga(msg.sender, enderecoUNIC, valorATransferir);
   } 
 
   function autoDestruicao() public payable {
