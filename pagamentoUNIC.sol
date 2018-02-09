dcf6b8033b8fad36265637ec520507ac5fdce728
Marcelo Creimer <14227072+mcreimer@users.noreply.github.com>
2018-02-09 16:08:48 -0200

    Alt. conta da UNIC + função autoDestruição
    Mudanças pequenas:
    1) Alterei a variavel enderecoUNIC para refletir um endereço da minha rede Private-Net. O endereço anterior era ficticio e invalido.
    2) Criei a função autoDestruição, que permite destruir o contrato e resgatar os fundos que existam nele

--- pagamentoUNIC.sol
+++ pagamentoUNIC.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.4.18;
 
 contract pagamentoUNIC {
 
-  address public enderecoUNIC = 0x06012c8cf97BEaD5deAe257070F9587f8E7A266d;  // UNIC = University of Nicosia
+  address public enderecoUNIC = 0x8977C1F513ca4C60d331B027911AFd23Aa370557;  // UNIC = University of Nicosia
   uint dataUltimaTransferencia;
   event Enviado(address remetente, address destinatario, uint valor);
 
@@ -10,15 +10,20 @@ contract pagamentoUNIC {
   	address enderecoRemetente = msg.sender;
   	uint valorATransferir = 1 ether;
 
-    // Se Paulo (msg.sender) nao tiver saldo de pelo menos 1 Ether, a função não fará nada
+    	// Se Paulo (msg.sender) nao tiver saldo de pelo menos 1 Ether, a função não fará nada
   	require(enderecoRemetente.balance >= valorATransferir);
 
-	  // Se a ultima transferencia foi feita a menos de 30 dias, não permitir nova transferencia  	 
-	  require(now >= dataUltimaTransferencia + 30 days); 
+	// Se a ultima transferencia foi feita a menos de 30 dias, não permitir nova transferencia  	 
+	require(now >= dataUltimaTransferencia + 30 days); 
 
    	enderecoUNIC.transfer(valorATransferir);  // Caso não consiga transferir, gera erro e não prossegue
    	
-	  dataUltimaTransferencia = now;	
-	  Enviado(msg.sender, enderecoUNIC, valorATransferir);
+	dataUltimaTransferencia = now;	
+	Enviado(msg.sender, enderecoUNIC, valorATransferir);
+  } 
+
+  function autoDestruicao() public payable {
+  	selfdestruct(msg.sender);
   }
+
 }
