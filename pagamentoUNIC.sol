9bc127bd999f490dda5b10f39e172793ec04c69d
Marcelo Creimer <14227072+mcreimer@users.noreply.github.com>
2018-02-16 15:32:02 -0200

    Adição de eventos e proprietario no self-destruct
    - Criado mais 2 eventos para monitorar eventuais falhas na transferencia
    - Gravação do dono do contrato no construtor
    - Troca das instruções "require" por "if" para poder monitorar os eventos
    - Inclusão do endereço do dono do contrarto no self-destruct (caso contrário, quem chamasse a destruição do contrato obteria os fundos para si)

--- pagamentoUNIC.sol
+++ pagamentoUNIC.sol
@@ -4,20 +4,29 @@ contract pagamentoUNIC {
 
   address public enderecoUNIC = 0x8977C1F513ca4C60d331B027911AFd23Aa370557;  // UNIC = University of Nicosia
   uint dataUltimaTransferencia;
+  address proprietario;
   event UnicFoiPaga(address enderecoQueDisparouContrato, address destinatario, uint valor);
+  event SaldoInsuficiente(address enderecoQueDisparouContrato, address destinatario, uint valorATransferir, saldo);
+  event UltimaTransferenciaMenor30Dias(address enderecoQueDisparouContrato, address destinatario, uint dataUltimaTransferencia);  
 
   function pagamentoUNIC() public payable {
+    proprietario = msg.sender;  
   }
   
   function transfereParaUNIC() public payable {
   	address enderecoRemetente = msg.sender;
   	uint valorATransferir = 1 ether;
 
-    	// Se o contrato nao tiver saldo de pelo menos 1 Ether, a função não fará nada
-  	require(this.balance >= valorATransferir);
+  	if(this.balance >= valorATransferir) {
+      		SaldoInsuficiente(msg.sender, enderecoUNIC, valorATransferir, this.balance);
+      		return;
+    	}
 
 	// Se a ultima transferencia foi feita a menos de 30 dias, não permitir nova transferencia  	 
-	require(now >= dataUltimaTransferencia + 30 days); 
+	if (now >= dataUltimaTransferencia + 30 days) {
+        	UltimaTransferenciaMenor30Dias(msg.sender, enderecoUNIC, dataUltimaTransferencia);
+        	return;
+    	}
 
    	enderecoUNIC.transfer(valorATransferir);  // Caso não consiga transferir, gera erro e não prossegue
    	
@@ -26,7 +35,7 @@ contract pagamentoUNIC {
   } 
 
   function autoDestruicao() public payable {
-  	selfdestruct(msg.sender);
+  	selfdestruct(proprietario);
   }
 
 }
