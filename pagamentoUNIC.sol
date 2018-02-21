356886a52b42ef4cb6128fee5a8a6483c4b6f0da
Marcelo Creimer <14227072+mcreimer@users.noreply.github.com>
2018-02-21 15:53:03 -0300

    Inclusão de 3 novas features
    1. Permitir alterar o endereço da faculdade, caso a faculdade mude seu endereço Ethereum
    2. Permitir alterar o valor a ser enviado, caso o valor da mensalidade aumente.
    3. Permitir transferências para endereços listados eu uma White list, já que Natalia pode vir a fazer um curso extra-curricular na Universidade, e este precisa ser pago direto para este departamento, em outro endereço Ethereum

--- pagamentoUNIC.sol
+++ pagamentoUNIC.sol
@@ -2,39 +2,87 @@ pragma solidity ^0.4.18;
 
 contract pagamentoUNIC {
 
-  address public enderecoUNIC = 0x8977C1F513ca4C60d331B027911AFd23Aa370557;  // UNIC = University of Nicosia
+  address public enderecoUNIC; // UNIC = University of Nicosia
+  address proprietario;  
+  
   uint dataUltimaTransferencia;
-  address proprietario;
+  uint valorATransferir;
+  
+  address[] whiteList;
+  
   event UnicFoiPaga(address enderecoQueDisparouContrato, address destinatario, uint valor);
   event SaldoInsuficiente(address enderecoQueDisparouContrato, address destinatario, uint valorATransferir, uint saldo);
-  event UltimaTransferenciaMenor30Dias(address enderecoQueDisparouContrato, address destinatario, uint dataUltimaTransferencia);  
+  event UltimaTransferenciaMenor30Dias(address enderecoQueDisparouContrato, address destinatario, uint dataUltimaTransferencia);
+  event ApenasProprietarioMudaEnderecoUnic();
+  event ApenasProprietarioMudaValorATransferir();
+  event NovoValor(uint valorAlterado);
+  event ElementoWhiteList(uint indice, address elemento);
+  event ElementoRemovido(address endereco)
 
   function pagamentoUNIC() public payable {
-    proprietario = msg.sender;  
+    proprietario = msg.sender;
+    dataUltimaTransferencia = 0;
+    valorATransferir = 1 ether;
+    enderecoUNIC = 0x8977C1F513ca4C60d331B027911AFd23Aa370557;
+  }
+
+  function mudaEnderecoUNIC(address _novoEndereco) public {
+    if (msg.sender == proprietario) {
+      enderecoUNIC = _novoEndereco;
+    } else {
+      ApenasProprietarioMudaEnderecoUnic();      
+    }
+  }
+
+  function alteraValorATransferir(uint _novoValor) public {
+    if (msg.sender == proprietario) {
+      valorATransferir = _novoValor;
+      NovoValor(valorATransferir);
+    } else {
+      ApenasProprietarioMudaValorATransferir();      
+    }   
+  }
+  
+  function adicionaEndereco(address _enderecoAAdicionar) public {
+      whiteList.push(_enderecoAAdicionar);
+  }
+  
+  function removeEndereco(address _enderecoARemover) public {
+    for (uint i = 0; i < whiteList.length-1; i++) {
+            if(whiteList[i] == _enderecoARemover) {
+              delete whiteList[i];
+              ElementoRemovido(whiteList[i]);
+            }
+    }
+  }
+
+  function listaWhiteList() public {
+    for (uint i = 0; i < whiteList.length-1; i++) {
+            ElementoWhiteList(i+1, whiteList[i]);
+    }
   }
   
   function transfereParaUNIC() public payable {
-  	uint valorATransferir = 1 ether;
+    // Se o contrato nao tiver saldo do valor da mensalidade, a função não fará nada
+    if(this.balance < valorATransferir) {
+      SaldoInsuficiente(msg.sender, enderecoUNIC, valorATransferir, this.balance);
+      return;
+    }
+    
+    // Se a ultima transferencia foi feita a menos de 30 dias, não permitir nova transferencia    
+    if (now < dataUltimaTransferencia + 30 days) {
+       UltimaTransferenciaMenor30Dias(msg.sender, enderecoUNIC, dataUltimaTransferencia);
+       return;
+    }
 
-  	if(this.balance < valorATransferir) {
-      		SaldoInsuficiente(msg.sender, enderecoUNIC, valorATransferir, this.balance);
-      		return;
-    	}
-
-	// Se a ultima transferencia foi feita a menos de 30 dias, não permitir nova transferencia  	 
-	if (now < dataUltimaTransferencia + 30 days) {
-        	UltimaTransferenciaMenor30Dias(msg.sender, enderecoUNIC, dataUltimaTransferencia);
-        	return;
-    	}
-
-   	enderecoUNIC.transfer(valorATransferir);  // Caso não consiga transferir, gera erro e não prossegue
-   	
-	dataUltimaTransferencia = now;	
-	UnicFoiPaga(msg.sender, enderecoUNIC, valorATransferir);
+    enderecoUNIC.transfer(valorATransferir);  // Caso não consiga transferir, gera erro e não prossegue
+    
+    dataUltimaTransferencia = now; 
+    UnicFoiPaga(msg.sender, enderecoUNIC, valorATransferir);
   } 
 
   function autoDestruicao() public payable {
-  	selfdestruct(proprietario);
+    selfdestruct(proprietario);
   }
 
 }
